e64e4e2c6c4c73748d5b7c76d1000edbfa5d2eef
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-09-10 00:33:52 +0100

    Add update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,10 +8,15 @@
  */
 int main(void)
 {
+        int num;
 
-	for (int num = 0; num <= 9; num++)
+        num = 0;
+  
+
+	while (num <= 9)
 	{
 		putchar(num + '0');
+		num++;
 	}
 
 	putchar('\n');
