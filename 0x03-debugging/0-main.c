86a38c86c5c9f82f9738b00aa062b202aa45ace2
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-09-14 13:55:25 +0000

    Debug issue with betty

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * main - this program tests a function that checks if a number is positive or
- * negative
- *
- * Return: 0 always
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
 	int i;
 
-	i = 99;
+	i = 98;
 	positive_or_negative(i);
 
 	return (0);
