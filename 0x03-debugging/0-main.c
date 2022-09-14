79cd17f44e4f0cf00824e33d7e3c9d9aafa23e59
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-09-14 13:08:19 +0000

    Start task one

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - this program tests a function that checks if a number is positive or
+ * negative
+ *
+ * Return: 0 always
+ */
+int main (void)
+{
+	int i;
+	i = 99;
+	positive_or_negative(i);
+
+	return (0);
+}
