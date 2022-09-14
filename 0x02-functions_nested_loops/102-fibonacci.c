73c9653dd5fe07e2d845320d1526a76103fe0fd3
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-09-14 17:39:55 +0000

    Add code for task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 numbers fibonacci in series
+ *
+ * Return: 0 always
+ */
+int main (void)
+{
+	long b1, b2, b3;
+	int n;
+
+	b1 = 1;
+	b2 = 2;
+
+	printf("%ld, %ld, ", b1, b2);
+
+	for (n = 2; n < 50; n++)
+	{
+		b3 = b2;
+		b2 += b1;
+		b1 = b3;
+
+		printf("%ld", b2);
+		if (n == 49)
+			continue;
+		printf(", ");
+	}
+
+	return (0);
+}
