c0a1f86952ff7701a9b221aac59d91d79e646103
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-09-14 12:26:53 +0000

    Alter code again for task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
  */
 void print_to_98(int n)
 {
-	printf("%d", n);
+	/*printf("%d", n);
 
 	if (n != 98)
 		printf(", ");
@@ -19,5 +19,16 @@ void print_to_98(int n)
 	else if (n < 98)
 		print_to_98(++n);
 	if (n == 98)
-		putchar('\n');
+		putchar('\n');*/
+
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		if (n < 98)
+			n++;
+		else if (n > 98)
+			n--;
+	}
+
+	printf("98\n");
 }
