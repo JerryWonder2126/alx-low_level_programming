eb324a9ded62107cecd8e8e5f6000ef941819c74
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-09-20 16:55:06 +0000

    Correct code to meet betty's standard

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * strlen - prints the length of a string
+ * _strlen - prints the length of a string
  *
  * @s: the string to be measured
  * Return: length of string
  */
-int strlen(char *s)
+int _strlen(char *s)
 {
 	int x = 0;
+
 	while (*s[x])
 		x++;
 	return (x);
