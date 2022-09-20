7122f32ddaa97e535b8e53a5ef7f5934f37c4e2a
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-09-20 15:24:15 +0000

    Add code for task 1 arrays project

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ *
+ * @a: value 1
+ * @b: value 2
+ */
+void swap_int(int *a, int *b)
+{
+	int *c = b;
+
+	b = a;
+	a = c;
+}
