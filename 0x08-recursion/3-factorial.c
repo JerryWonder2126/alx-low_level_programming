0da7f8b3ce3d0f1d72a6f940dd173e167101f10f
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-09-29 10:49:26 +0100

    Task 3, unfinished

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - prints the factorial of a number
+ *
+ * @n: the number to be calculated
+ * Return: the factorial of a number
+ */
+int factorial(int n)
+{
+	int l = 1;
+
+	if (n < 0)
+		return (-1);
+
+	l = _strlen_recursion(s + 1);
+	return (l + 1);
+}
