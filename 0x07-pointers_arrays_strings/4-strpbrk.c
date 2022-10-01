137e9e585b6ef358020df90b6523bc21f3ef4923
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-10-01 18:08:28 +0100

    Task 4, first attempt

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - returns string s from first occurence of any character in accept
+ *
+ * @s: string to be checked
+ * @accept: bytes being checked against
+ * Return: char always
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int found = 0;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+
+		s++;
+	}
+
+	return ('\0');
+}
