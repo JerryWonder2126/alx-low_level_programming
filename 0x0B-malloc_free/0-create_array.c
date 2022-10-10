e3cf4eb653dab458ab862342591b043795915e11
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-10-10 15:47:52 +0000

    Task 0, correct code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an allocated space to store an array of size 'size' and initializes it with char c
+ * create_array - creates an allocated space to store an array of size 'size'
+ *                and initializes it with char c
  *
  * @size: the size of the array
  * @c: the character used to fill the array
- * Return: NULL if size is negative, zero or if the function fails, else the allocated space created
+ * Return: NULL if size is negative, zero or if the function fails
+ *         else the allocated space created
  */
 char *create_array(unsigned int size, char c)
 {
@@ -27,4 +29,3 @@ char *create_array(unsigned int size, char c)
 
 	return (mem);
 }
-
