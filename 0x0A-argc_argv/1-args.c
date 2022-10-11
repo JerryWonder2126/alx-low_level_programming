ff518d36cbb1239ee659a6cdcb29bd5d2d387eae
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-10-11 17:39:13 +0000

    Task 1, first attempt

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this programs prints the number of command line arguments
+ *        passed to this script
+ *
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: integer always
+ */
+int main(int argc, char __attribute__((__unused__)) **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
