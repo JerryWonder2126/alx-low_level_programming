da332d63a869feda263062d75d9c52dab7a351c8
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-12-12 21:27:09 +0000

    First attempt, task 2

---
+++ 0x18-dynamic_libraries/3-operators.c
@@ -0,0 +1,59 @@
+/**
+ * add - returns the sum of a and b.
+ *
+ * @a: number 1
+ * @b: number 2
+ * Return: result of operation
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - returns the difference of a and b.
+ *
+ * @a: number 1
+ * @b: number 2
+ * Return: result of operation
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - returns the product of a and b.
+ *
+ * @a: number 1
+ * @b: number 2
+ * Return: result of operation
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - returns the result of the division of a by b.
+ *
+ * @a: number 1
+ * @b: number 2
+ * Return: result of operation
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns the remainder of the division of a by b.
+ *
+ * @a: number 1
+ * @b: number 2
+ * Return: result of operation
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
