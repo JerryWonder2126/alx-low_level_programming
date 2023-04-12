1932e05f4480db06a86d3dcb5d496617a21d1c68
Jerry Wonder <jerrycul2001@gmail.com>
2023-04-12 23:56:18 +0100

    feat: work on binary search function

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,11 @@
 #include "search_algos.h"
 
+void print_array(int *array, size_t start, size_t end);
+
+
 /**
- * binary_search - searches for a value in a sorted array of integers using the Binary search algorithm
+ * binary_search - searches for a value in a sorted array of integers
+ *                 using the Binary search algorithm
  *
  * @array: the array to be searched
  * @size: the size of the array
@@ -23,7 +27,32 @@ int binary_search(int *array, size_t size, int value)
 	/* back to the beginning; */
 
 	size_t start = 0;
-	size_t end = 0;
+	size_t end = size;
+	size_t index = (end - start)/2;
+
+	if (array == NULL)
+		return (-1);
+
+	while (index > 1 && index < end - 1)
+	{
+		/* print_array(array, start, end);*/
+		printf("%lu\n", index);
+		if (array[index] == value)
+			return (index);
+		else if (array[index] < value)
+			index = ((end - index)/2) + index;
+		else
+			index = (index - start)/2;
+	}
+
+	return (-1);
 }
 
-recurse_binary_search(int *array, size_t index, size_t value)
+void print_array(int *array, size_t start, size_t end)
+{
+	size_t i = start;
+
+	while (i < end - 1)
+		printf("%d, ", array[i++]);
+	printf("%d\n", array[i]);
+}
