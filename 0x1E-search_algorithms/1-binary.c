6710cc569437ef78912a2834740179ca20b629a6
Jerry Wonder <jerrycul2001@gmail.com>
2023-04-12 20:44:56 +0100

    feat: add building blocks for binary search function

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array of integers using the Binary search algorithm
+ *
+ * @array: the array to be searched
+ * @size: the size of the array
+ * @value: the value to be searched
+ * Return: the index wher value is located
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	/* let index = array/2; */
+	/* start = 0; */
+	/* end = size; */
+	/* index = (end - start) / 2; */
+	/* Check if the element at index is equal to value; */
+	/* return value; */
+	/* else check if value is greater, if greater; */
+	/* start = index; */
+	/* else check if value is lower, if lower; */
+	/* end = index; */
+	/* back to the beginning; */
+
+	size_t start = 0;
+	size_t end = 0;
+}
+
+recurse_binary_search(int *array, size_t index, size_t value)
