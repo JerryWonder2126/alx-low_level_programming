d761394fb42bc21c9448596088fe6a5172f8d0d0
Jerry Wonder <jerrycul2001@gmail.com>
2023-04-13 09:15:32 +0100

    feat: complete binary_search function

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,22 +10,10 @@ void print_array(int *array, size_t start, size_t end);
  * @array: the array to be searched
  * @size: the size of the array
  * @value: the value to be searched
- * Return: the index wher value is located
+ * Return: the index where value is located or -1 if value not found
  */
 int binary_search(int *array, size_t size, int value)
 {
-	/* let index = array/2; */
-	/* start = 0; */
-	/* end = size; */
-	/* index = (end - start) / 2; */
-	/* Check if the element at index is equal to value; */
-	/* return value; */
-	/* else check if value is greater, if greater; */
-	/* start = index; */
-	/* else check if value is lower, if lower; */
-	/* end = index; */
-	/* back to the beginning; */
-
 	size_t start = 0;
 	size_t end = size;
 	size_t index = (end - start)/2;
@@ -33,21 +21,33 @@ int binary_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	while (index > 1 && index < end - 1)
+	while (index <= size)
 	{
-		/* print_array(array, start, end);*/
-		printf("%lu\n", index);
-		if (array[index] == value)
+		print_array(array, start, end);
+	        if (array[index] == value)
 			return (index);
 		else if (array[index] < value)
-			index = ((end - index)/2) + index;
+		{
+			start = index;
+			index = (end - start)/2 + index;
+		}
 		else
-			index = (index - start)/2;
+		{
+			end = index;
+			index = (end - start)/2;
+		}
 	}
 
 	return (-1);
 }
 
+/**
+ * print_array - prints every entries in an array
+ *
+ * @array: the array to be printed
+ * @start: the index to start printing from
+ * @end: the index to stop printing at
+ */
 void print_array(int *array, size_t start, size_t end)
 {
 	size_t i = start;
